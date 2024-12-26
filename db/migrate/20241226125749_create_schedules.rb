45e698546555ab4b8d6b8de4d7571ee90a9ad9f5
stskri <177446189+stskri@users.noreply.github.com>
2024-12-26 14:16:31 +0000

    Add migration-file

--- db/migrate/20241226125749_create_schedules.rb
+++ db/migrate/20241226125749_create_schedules.rb
@@ -2,6 +2,10 @@ class CreateSchedules < ActiveRecord::Migration[6.1]
   def change
     create_table :schedules do |t|
 
+      t.integer :user_id, null: false
+      t.string :name, null: false
+      t.string :explanation
+
       t.timestamps
     end
   end
--- db/migrate/20241226125831_create_lessons.rb
+++ db/migrate/20241226125831_create_lessons.rb
@@ -1,6 +1,14 @@
 class CreateLessons < ActiveRecord::Migration[6.1]
   def change
     create_table :lessons do |t|
+      t.integer :schedule_id, null: false
+      t.integer :subject_id, null: false
+      t.integer :day_of_week, null: false # enum
+      t.integer :turn, null: false
+      t.string :teacher_name
+      t.time :start_time
+      t.time :end_time
+      t.string :room_name
 
       t.timestamps
     end
