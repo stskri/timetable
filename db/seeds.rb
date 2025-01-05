b0b7503234f5f67760391dbf2bce5e8c38954770
stskri <177446189+stskri@users.noreply.github.com>
2025-01-05 14:40:47 +0000

    Add a seed for Subject

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seedを作成します。"
+
+subjects = [
+  { id: 1, name: "国語", color: "#c32148" },
+  { id: 2, name: "算数", color: "#4169e1" },
+  { id: 3, name: "理科", color: "#3cb737" },
+  { id: 4, name: "社会", color: "#8b4513" },
+  { id: 5, name: "英語", color: "#ffa500" }
+]
+subjects.each do |subject_data|
+  Subject.find_or_create_by(id: subject_data[:id]) do |subject|
+    subject.name = subject_data[:name]
+    subject.color = subject_data[:color]
+  end
+end
+
+puts "seedの作成が完了しました。"
