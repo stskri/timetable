df195c136a4bc2d673db917007fe4bde96882d8b
stskri <177446189+stskri@users.noreply.github.com>
2025-01-08 16:18:24 +0000

    Nest lessons in schedules

--- app/controllers/public/lessons_controller.rb
+++ app/controllers/public/lessons_controller.rb
@@ -1,9 +1,14 @@
 class Public::LessonsController < ApplicationController
   def new
     @subjects = Subject.all
+    @lesson = Lesson.new
   end
 
   def create
-    
+    @lesson = Lesson.new
+    @lesson.subject_id = params[:lesson][:selected_subject_id]
+    if @lesson.save
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root :to => "schedules#index"
-    resources :schedules
-    resources :lessons
+    resources :schedules do
+      resources :lessons
+    end
   end
 
   namespace :admin do
