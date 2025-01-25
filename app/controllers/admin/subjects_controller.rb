7181a37c96af88ac04ec67398738a7f60132d997
stskri <177446189+stskri@users.noreply.github.com>
2025-01-25 13:14:59 +0000

    Add admin/subjects#update

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -6,4 +6,20 @@ class Admin::SubjectsController < ApplicationController
   def edit
     @subject = Subject.find(params[:id])
   end
+
+  def update
+    subject = Subject.find(params[:id])
+    if subject.update(subject_params)
+      redirect_to admin_subjects_path, notice: "科目情報を更新しました"
+    else
+      redirect_to edit_admin_subject_path(subject), alert: "科目情報の更新に失敗しました"
+    end
+  end
+
+
+
+  private
+  def subject_params
+    params.require(:subject).permit(:name, :color)
+  end
 end
