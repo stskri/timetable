2e119c7cf0b88f0208f6208d2c70318f353d44c5
stskri <177446189+stskri@users.noreply.github.com>
2025-01-26 14:34:48 +0000

    Add character limit to public side

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,5 @@ class Schedule < ApplicationRecord
   has_many :lessons, dependent: :destroy
 
   validates :name, length: { maximum: 20 }, presence: true
+  validates :explanation, length: { maximum: 30 }, presence: true
 end
